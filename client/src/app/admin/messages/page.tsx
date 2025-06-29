f4ffa4e3e244396686b4393303286804501d0d1c
Name <user@example.com>
2025-06-29 23:13:26 +0545

    updating message section

--- client/src/app/admin/messages/page.tsx
+++ client/src/app/admin/messages/page.tsx
@@ -89,7 +89,7 @@ const Messages: React.FC = () => {
 
       {messages.length > 0 ? (
         <>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6">
             {currentMessages.map((msg, index) => (
               <div
                 key={msg.Email + msg.DateTime + index}
